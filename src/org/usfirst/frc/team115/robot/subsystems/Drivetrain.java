536c23fd2a547a14cfce0748e503dc0780968ca0
Hee Won Chung <heewonchung64@gmail.com>
2018-09-14 16:53:35 -0700

    Added Safety and Voltage Compensation

--- src/org/usfirst/frc/team115/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team115/robot/subsystems/Drivetrain.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Drivetrain extends Subsystem{
 
-	WPI_VictorSPX first, second, third, fourth;		
+	WPI_VictorSPX first, second, third, fourth;	
 	
 	public Drivetrain() {
 		first = new WPI_VictorSPX(7);
@@ -21,32 +21,64 @@ public class Drivetrain extends Subsystem{
 		fourth = new WPI_VictorSPX(10);
 	}
 	
+	public void talonInit()
+	{
+		first.configVoltageCompSaturation(10.5, 50);
+		first.enableVoltageCompensation(true);
+		
+		second.configVoltageCompSaturation(10.5, 50);
+		second.enableVoltageCompensation(true);
+		
+		third.configVoltageCompSaturation(10.5, 50);
+		third.enableVoltageCompensation(true);
+		
+		fourth.configVoltageCompSaturation(10.5, 50);
+		fourth.enableVoltageCompensation(true);	
+	}
+	
 	public void drive() {
 		Robot.oi.getButton();
-		while(Robot.oi.firstToggle) {
-			first.set(Robot.oi.getThrottle());
-			Robot.oi.getButton();
-		}
 		
-		while(Robot.oi.secondToggle) {
-			second.set(Robot.oi.getThrottle());
-			Robot.oi.getButton();
-		}
+		if(Robot.oi.joystick.getRawButton(1))
+		{
+			if(Robot.oi.firstToggle) {
+				first.set(Robot.oi.getThrottle());
+				Robot.oi.getButton();
+			}
+			
+			if(Robot.oi.secondToggle) {
+				second.set(Robot.oi.getThrottle());	
+				Robot.oi.getButton();
+
+			}
+			
+			if(Robot.oi.thirdToggle) {
+				third.set(Robot.oi.getThrottle());
+				Robot.oi.getButton();
+			}
+			
+			if(Robot.oi.fourthToggle) {
+				fourth.set(Robot.oi.getThrottle());
+				Robot.oi.getButton();
+			}
 		
-		while(Robot.oi.thirdToggle) {
-			third.set(Robot.oi.getThrottle());
-			Robot.oi.getButton();
 		}
-		
-		while(Robot.oi.fourthToggle) {
-			fourth.set(Robot.oi.getThrottle());
-			Robot.oi.getButton();
+		else
+		{
+			first.set(0);
+			second.set(0);
+			third.set(0);
+			fourth.set(0);
 		}
-		
 	}
 	
 	protected void initDefaultCommand() {
 		setDefaultCommand(new DriveWithJoystick());
+	}
+	
+	public WPI_VictorSPX getFirst()
+	{
+		return first;
 	}
 
 }
